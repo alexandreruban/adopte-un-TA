3767ad6021af5ee2002e4947bdc45ab74d087a1c
Alexandre Ruban <alexandre.ruban@gmail.com>
2018-05-21 15:49:10 +0200

    Added associations to booking model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,7 @@
 class Course < ApplicationRecord
+  has_many :bookings
   belongs_to :user
+
   validates :title, presence: true
   validates :price, presence: true
   validates :address, presence: true
