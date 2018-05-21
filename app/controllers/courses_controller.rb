4570d3ef336924066501a99b174d9b4ebf81977d
Guillaume Legrand <gjlegrandfr@gmail.com>
2018-05-21 17:14:25 +0200

    Add controller/views for courses

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,11 @@
+class CoursesController < ApplicationController
+    skip_before_action :authenticate_user!, only: :index
+
+  def index
+    @courses = Course.all
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+end
