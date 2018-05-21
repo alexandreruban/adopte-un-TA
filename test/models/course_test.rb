cdbd52fe64aa93733475915c1030195a54403206
Alexandre Ruban <alexandre.ruban@gmail.com>
2018-05-21 22:06:37 +0200

    created tests and validations for courses model

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,43 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:seb)
+    @course = courses(:ruby_on_rails)
+    @course.user = @user
+  end
+
+  test "course should be valid" do
+    assert @course.valid?
+  end
+
+  test "course should have a title" do
+    @course.title = "   "
+    assert_not @course.valid?
+  end
+
+  test "course should have an address" do
+    @course.address = "   "
+    assert_not @course.valid?
+  end
+
+  test "course should have a description" do
+    @course.description = "   "
+    assert_not @course.valid?
+  end
+
+  test "course should have a begin_date" do
+    @course.begin_date = ""
+    assert_not @course.valid?
+  end
+
+  test "course should have a end_date" do
+    @course.end_date = ""
+    assert_not @course.valid?
+  end
+
+  test "start date should be before end date" do
+    @course.end_date = Date.today - 1
+    assert_not @course.valid?
+  end
 end
