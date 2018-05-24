5695ecfe39562d583fd13edf7a3ab8d2b8529890
Remi Mercier <remi.merci@gmail.com>
2018-05-24 15:29:25 +0200

    Pass GitHub image url through Cloudinary API

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,8 +2,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
       # You need to implement the method below in your model (e.g. app/models/user.rb)
       @user = User.from_omniauth(request.env["omniauth.auth"])
-
       if @user.persisted?
+        unless @user.avatar.present?
+          photo_url = request.env["omniauth.auth"].info.image
+          @user.remote_avatar_url = photo_url
+          @user.save!
+        end
         sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
         set_flash_message(:notice, :success, kind: "GitHub") if is_navigational_format?
       else
