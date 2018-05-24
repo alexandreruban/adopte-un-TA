3db7dcde5945e46211ff9f20be4a8d7bd6e2e5a7
Remi Mercier <remi.merci@gmail.com>
2018-05-24 17:28:24 +0200

    Handle case when user tries to connect with GitHub while already being in DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,22 +9,32 @@ class User < ApplicationRecord
   has_many :courses, dependent: :destroy
   has_many :bookings
 
-  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: EMAIL_REGEX }
-  validates :password, presence: true
 
   def self.from_omniauth(auth)
-  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    user.email = auth.info.email
-    user.password = Devise.friendly_token[0,20]
-    user.first_name = auth.info.name.split(' ').first  # assuming the user model has a name
-    user.last_name = auth.info.name.split(' ').second  # assuming the user model has a name
-    # user.avatar = auth.info.image # assuming the user model has an image
-    # If you are using confirmable and the provider(s) you use validate emails,
-    # uncomment the line below to skip the confirmation emails.
-    # user.skip_confirmation!
+    user = User.find_by(email: auth.info.email)
+    if user
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.save
+    else
+      user = User.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+        user.first_name = auth.info.name.split(' ').first  # assuming the user model has a name
+        user.last_name = auth.info.name.split(' ').second  # assuming the user model has a name
+        # user.avatar = auth.info.image # assuming the user model has an image
+        # If you are using confirmable and the provider(s) you use validate emails,
+        # uncomment the line below to skip the confirmation emails.
+        # user.skip_confirmation!
+      end
+    end
+    unless user.avatar.present?
+      photo_url = auth.info.image
+      user.remote_avatar_url = photo_url
+      user.save
+    end
+    user
   end
-end
 end
