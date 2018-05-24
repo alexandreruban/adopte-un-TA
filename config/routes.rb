7e061f5aa18a521902a3bf27a1d7dccff7f308aa
Remi Mercier <remi.merci@gmail.com>
2018-05-24 15:31:35 +0200

    Remove conflicting devise scope for logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  # devise_scope :user do
-  #   delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
-  # end
 
   resources :courses do
     resources :bookings, only: :create
