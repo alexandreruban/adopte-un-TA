44c1a818ab6d3c6ceed8bbc47dd2de41c9fbcf1b
Guillaume Legrand <gjlegrandfr@gmail.com>
2018-05-24 14:36:46 +0200

    Add map to course page

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,7 @@
 class Course < ApplicationRecord
   mount_uploader :photo, PhotoUploader
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   has_many :bookings
   belongs_to :user
