659a5ec852aeaf2b7583135b4bf84e8084f8dc05
Alexandre Ruban <alexandre.ruban@gmail.com>
2018-05-25 12:50:06 +0200

    created a nice button for the input file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,8 @@
 import "bootstrap";
 import { addClickAvatar, togglerNav } from "./components/navbar.js";
+import { inputFileUpdate } from "./components/file_input.js";
 import "../plugins/flatpickr"
 
 addClickAvatar();
 togglerNav();
+inputFileUpdate();
